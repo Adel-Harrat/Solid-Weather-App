c75a1d2484e020200670afb343b7b2bbede1c37a
Adel-Harrat <adoulamst@gmail.com>
2024-09-18 19:03:12 +0100

    better fetching

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,55 +1,48 @@
 import { Title } from "@solidjs/meta";
-import { For, Match, Switch, createResource } from "solid-js";
-import { isServer } from "solid-js/web";
-
-// Weather api string
-// http://api.weatherapi.com/v1/forecast.json?key=c8d5a3373bb74ef591a173934241409&q=Mostaganem&days=7&aqi=no&alerts=no
-
-type Post = {
-  id: string;
-  title: string;
-  body: string;
-};
-
-const fetchPosts = async (): Promise<Post[]> => {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=5"
-  );
-  if (!response.ok) throw new Error("Error when fetching!");
-  return await response.json();
-};
+import {
+  ErrorBoundary,
+  For,
+  Match,
+  Suspense,
+  Switch,
+  createEffect,
+  createResource,
+} from "solid-js";
+import { AiOutlineLoading3Quarters } from "solid-icons/ai";
+import { ImSpinner9 } from "solid-icons/im";
+// Create ui with static data
+// Present data within that ui
+// DONE : --------------------
+// Hide API key
 
 export default function Home() {
-  const [posts, { refetch }] = createResource<Post[]>(fetchPosts);
+  const [weatherData, { refetch }] = createResource(async () => {
+    const response = await fetch(
+      "http://api.weatherapi.com/v1/forecast.json?key=c8d5a3373bb74ef591a173934241409&q=Mostaganem&days=7&aqi=no&alerts=no"
+    );
+    if (!response.ok) throw new Error("Error when fetching!");
+    return await response.json();
+  });
 
   return (
     <main class="p-10">
       <Title>Solid Weather App</Title>
-      <h1 class="text-3xl text-center p-5">Home Page</h1>
-      <button onclick={() => refetch()}>Reload posts</button>
 
-      <Switch>
-        <Match when={posts.state === "pending" || posts.state === "unresolved"}>
-          <div>Fetching Posts ...</div>
-        </Match>
-        <Match when={posts.state === "ready"}>
-          <section class="space-y-5 rounded">
-            <For each={posts()}>
-              {(post) => (
-                <article class="bg-gray-50/50 p-5 space-y-2 capitalize">
-                  <h2 class="font-extrabold tracking-tight text-2xl ">
-                    {post.title}
-                  </h2>
-                  <p>{post.body}</p>
-                </article>
-              )}
-            </For>
-          </section>
-        </Match>
-        <Match when={posts.state === "errored"}>
-          <div>Sorry, but we couldn't fetch posts.</div>
-        </Match>
-      </Switch>
+      <ErrorBoundary
+        fallback={
+          <div class="text-red-600 font-bold">{weatherData.error?.message}</div>
+        }
+      >
+        <Suspense
+          fallback={
+            <div>
+              <ImSpinner9 class="animate-spin text-2xl text-blue-500" />
+            </div>
+          }
+        >
+          <div>Data is Ready.</div>
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
